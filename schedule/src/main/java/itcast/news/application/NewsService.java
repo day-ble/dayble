3ec410d024ac2ca2d6bdc751254977f140bc1305
jubseok <junseok708@gmail.com>
2024-12-09 15:42:48 +0900

    [#2] feat : 필요 없는 문자 삭제 및 출판일 수정 메소드 작성

--- schedule/src/main/java/itcast/news/application/NewsService.java
+++ schedule/src/main/java/itcast/news/application/NewsService.java
@@ -7,28 +7,32 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class NewsService {
 
     @Value("${spring.crawler.naver-it-url}")
-    private String naverUrl;
+    private String Url;
 
-    public List<String> newsCrawling() throws IOException {
-        Document document = Jsoup.connect(naverUrl).get();
+    public void newsCrawling() throws IOException {
+        Document document = Jsoup.connect(Url).get();
         Elements articles = document.select(".sa_thumb_inner");
 
         List<String> links = new ArrayList<>();
         articles.forEach(article -> {
+            if (links.size() >= 10) {
+                return;
+            }
+
             String link = article.select("a").attr("href");
             links.add(link);
         });
 
         links.forEach(link -> {
-
             try {
                 Document url = Jsoup.connect(link).get();
                 String titles = url.select("#title_area").text();
@@ -38,12 +42,17 @@ public class NewsService {
                 String thumbnail =
                         url.selectFirst("meta[property=og:image]").attr("content");
 
-                // 저장 부분
+                titles = cleanContent(titles);
+                content = cleanContent(content);
+                LocalDateTime sendAt = convertDateTime(date);
+
+                // dto 저장
                 System.out.println(link);
                 System.out.println(titles);
                 System.out.println(content);
-                System.out.println(date);
-                if (thumbnail.isEmpty()){
+                System.out.println(sendAt);
+
+                if (thumbnail.isEmpty()) {
                     System.out.println("썸네일이 없습니다");
                 }
                 System.out.println(thumbnail);
@@ -51,6 +60,32 @@ public class NewsService {
                 throw new RuntimeException(e);
             }
         });
-        return links;
+    }
+
+    private LocalDateTime convertDateTime(String info) {
+        String[] parts = info.split(" ");
+        String date = parts[0];
+        String ampm = parts[1];
+        String time = parts[2];
+
+        date = date.replaceAll("입력", "");
+        String[] timeParts = time.split(":");
+        int hour = Integer.parseInt(timeParts[0]);
+
+        if (ampm.equals("오후") && hour != 12) {
+            hour += 12;
+        }
+
+        String timeDate = date + " " + String.format("%02d", hour) + ":" + timeParts[1];
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd. HH:mm");
+        LocalDateTime localDateTime = LocalDateTime.parse(timeDate, formatter);
+        return localDateTime;
+    }
+
+    private String cleanContent(String info) {
+        info = info.replaceAll("\\[.*?\\]", "")
+                .replaceAll("\\(.*?\\)", "")
+                .trim();
+        return info;
     }
 }
