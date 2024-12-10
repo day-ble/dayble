19ff1f9854c7c141d88324ea27ad66a86ee2a71b
jubseok <junseok708@gmail.com>
2024-12-10 16:10:16 +0900

    [#26] feat : 배치 어노테이션 위치 변경

--- schedule/src/main/java/itcast/ScheduleApplication.java
+++ schedule/src/main/java/itcast/ScheduleApplication.java
@@ -1,14 +1,12 @@
 package itcast;
 
 import itcast.news.application.NewsService;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableBatchProcessing
 public class ScheduleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScheduleApplication.class, args);
