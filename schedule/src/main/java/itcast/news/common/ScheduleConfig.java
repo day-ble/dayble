ed00e891ebdbe760835b100dd9ba3da1ccda73bf
jubseok <junseok708@gmail.com>
2024-12-10 10:18:54 +0900

    [#26] feat : issue26 자동화 생성

---
+++ schedule/src/main/java/itcast/news/common/ScheduleConfig.java
@@ -0,0 +1,10 @@
+package itcast.news.common;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ScheduleConfig {
+
+}
