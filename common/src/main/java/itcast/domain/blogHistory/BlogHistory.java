b32c4f2f6b47542e2568bf9d2832e09c7cf02924
jubseok <junseok708@gmail.com>
2024-12-11 13:06:22 +0900

    [#28] feat : 잘못된 삭제 수정

---
+++ common/src/main/java/itcast/domain/blogHistory/BlogHistory.java
@@ -0,0 +1,33 @@
+package itcast.domain.blogHistory;
+
+import itcast.domain.BaseEntity;
+import itcast.domain.news.News;
+import itcast.domain.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BlogHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "news_id")
+    private News news;
+}
