9c1b86ee29e2fe90125ff5dbc4b44a50f92a1d0c
jubseok <junseok708@gmail.com>
2024-12-11 12:45:44 +0900

    [#28] fix : 임포트 수정

--- schedule/src/main/java/itcast/news/common/schedule/NewsSchedule.java
+++ schedule/src/main/java/itcast/news/common/schedule/NewsSchedule.java
@@ -2,19 +2,10 @@ package itcast.news.common.schedule;
 
 import itcast.news.application.NewsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
@@ -26,5 +17,6 @@ public class NewsSchedule {
     public void scheduleNewsCrawling() throws IOException {
         System.out.println("crawling....");
         newsService.newsCrawling();
+        System.out.println("crawled End");
     }
 }
