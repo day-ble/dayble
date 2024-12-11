03725751a6e482ad07abcfdeed84fa48862663db
jubseok <junseok708@gmail.com>
2024-12-11 12:37:39 +0900

    [#28] fix : 배치 삭제 필요 없는 entity 삭제

--- schedule/src/main/java/itcast/news/common/schedule/NewsSchedule.java
+++ schedule/src/main/java/itcast/news/common/schedule/NewsSchedule.java
@@ -1,36 +1,21 @@
 package itcast.news.common.schedule;
 
+import itcast.news.application.NewsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-
+import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
 public class NewsSchedule {
-    @Qualifier("crawlNewsJob")
-    private final Job crawlingJob;
-    private final JobLauncher jobLauncher;
+    private NewsService newsService;
 
     @Scheduled(cron = "${spring.scheduler.cron.crawl-every-3-hour}")
-    public void scheduleNewsCrawling() throws
-            JobInstanceAlreadyCompleteException,
-            JobExecutionAlreadyRunningException,
-            JobParametersInvalidException,
-            JobRestartException {
+    public void scheduleNewsCrawling() throws IOException {
         System.out.println("crawling....");
-        jobLauncher.run(crawlingJob,new JobParametersBuilder()
-                .addDate("date", new Date())
-                .toJobParameters());
+        newsService.newsCrawling();
+        System.out.println("crawling finished.");
     }
 }
