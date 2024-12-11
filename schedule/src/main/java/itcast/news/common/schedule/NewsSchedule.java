10f9c7535d9d6c71144f6c5a14c1a30bf34db9c9
jubseok <junseok708@gmail.com>
2024-12-11 12:49:25 +0900

    [#26] fix : 임포트 수정, cron명 수정

--- schedule/src/main/java/itcast/news/common/schedule/NewsSchedule.java
+++ schedule/src/main/java/itcast/news/common/schedule/NewsSchedule.java
@@ -2,19 +2,10 @@ package itcast.news.common.schedule;
 
 import itcast.news.application.NewsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
@@ -22,9 +13,10 @@ public class NewsSchedule {
 
     private final NewsService newsService;
 
-    @Scheduled(cron = "${spring.scheduler.cron.news}")
+    @Scheduled(cron = "${spring.scheduler.cron.news-crawling}")
     public void scheduleNewsCrawling() throws IOException {
         System.out.println("crawling....");
         newsService.newsCrawling();
+        System.out.println("crawling finished.");
     }
 }
