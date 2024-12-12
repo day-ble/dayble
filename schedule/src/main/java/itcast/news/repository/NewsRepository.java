2e720994bdcbd57b1d8e07dfad717b2fa49f22de
jubseok <junseok708@gmail.com>
2024-12-12 15:06:30 +0900

    [#37] feat : 어제 크롤링한 내용 가져오기

--- schedule/src/main/java/itcast/news/repository/NewsRepository.java
+++ schedule/src/main/java/itcast/news/repository/NewsRepository.java
@@ -3,10 +3,14 @@ package itcast.news.repository;
 import itcast.domain.news.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
    @Query("select n.link from News n")
     List<String> findAllLinks();
+    @Query("select n from News n where function('DATE',n.createdAt) = :yesterday ")
+    List<News> findAllByCreatedAt(@Param("yesterday") LocalDate yesterday);
 }
