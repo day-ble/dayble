3dabd1a6aeab8ef642ee99b6beea21c3245701ee
jubseok <junseok708@gmail.com>
2024-12-18 09:55:28 +0900

    [#69] feat: 선택 및 발송 스케줄러 생성

--- schedule/src/main/java/itcast/news/common/schedule/AlarmSchedule.java
+++ schedule/src/main/java/itcast/news/common/schedule/AlarmSchedule.java
@@ -1,6 +1,7 @@
 package itcast.news.common.schedule;
 
 import itcast.news.application.NewsService;
+import itcast.news.application.SendNewsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,11 +12,26 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AlarmSchedule {
     private final NewsService newsService;
+    private final SendNewsService sendNewsService;
 
     @Scheduled(cron = "${scheduler.news.alarm-scheduled}")
     public void CreateAlarmSchedule() {
         log.info("alarm schedule....");
         newsService.newsAlarm();
         log.info("alarm schedule Finish");
+    }
+
+    @Scheduled(cron = "${scheduler.news.select-news}")
+    public void selectNewsSchedule() {
+        log.info("Selecting schedule....");
+        sendNewsService.selectNews();
+        log.info("Selecting Finish");
+    }
+
+    @Scheduled(cron = "${scheduler.news.send-alarm}")
+    public void sendAlarmSchedule() {
+        log.info("Sending schedule....");
+        sendNewsService.sendNews();
+        log.info("Sending schedule Finish");
     }
 }
