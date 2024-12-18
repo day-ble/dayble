c400286c127d087a29ceab0b574c2e9fa24125fd
jubseok <junseok708@gmail.com>
2024-12-18 17:57:12 +0900

    [#69] test: selectNews 메소드 테스트

--- schedule/src/test/java/itcast/news/application/SelectNewsServiceTest.java
+++ schedule/src/test/java/itcast/news/application/SelectNewsServiceTest.java
@@ -1,15 +1,21 @@
 package itcast.news.application;
 
+import itcast.domain.news.News;
 import itcast.domain.user.User;
 import itcast.domain.user.enums.Interest;
 import itcast.jwt.repository.UserRepository;
+import itcast.news.repository.NewsRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,9 +24,15 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class SelectNewsServiceTest {
+    private static final int YESTERDAY = 1;
+    private static final int ALARM_HOUR = 2;
+    private static final int ALARM_DAY = 2;
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private NewsRepository newsRepository;
 
     @InjectMocks
     private SendNewsService sendNewsService;
@@ -51,5 +63,34 @@ public class SelectNewsServiceTest {
         verify(userRepository, times(1)).findAllByInterest(validInterest);
         verify(mockUser1, times(1)).getEmail();
         verify(mockUser2, times(1)).getEmail();
+    }
+
+    @Test
+    @DisplayName("selectNews 메소드 테스트")
+    public void selectNewsTest() {
+        // give
+        LocalDate yesterday = LocalDate.now().minusDays(YESTERDAY);
+        LocalDateTime expectedSendAt = LocalDateTime.now().plusDays(ALARM_DAY).plusHours(ALARM_HOUR);
+
+        News mockNews1 = mock(News.class);
+        News mockNews2 = mock(News.class);
+        List<News> mockNewsList = List.of(mockNews1, mockNews2);
+
+        when(newsRepository.findRatingTot3ByCreatedAtOrdarByRating(yesterday)).thenReturn(mockNewsList);
+
+        // when
+        sendNewsService.selectNews();
+
+        // then
+        ArgumentCaptor<LocalDateTime> captor = ArgumentCaptor.forClass(LocalDateTime.class);
+
+        verify(mockNews1, times(1)).newsUpdate(captor.capture());
+        LocalDateTime actualSendAt1 = captor.getValue();
+        assertEquals(expectedSendAt.truncatedTo(ChronoUnit.SECONDS), actualSendAt1.truncatedTo(ChronoUnit.SECONDS));
+
+        verify(mockNews2, times(1)).newsUpdate(captor.capture());
+        LocalDateTime actualSendAt2 = captor.getValue();
+        assertEquals(expectedSendAt.truncatedTo(ChronoUnit.SECONDS), actualSendAt2.truncatedTo(ChronoUnit.SECONDS));
+
     }
 }
