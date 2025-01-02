b77160b3eee24c3409fe9844e721a06fb53a7923
SeonJuuuun <110665247+SeonJuuuun@users.noreply.github.com>
2025-01-02 10:25:57 +0900

    refactor: 메일 발송 로직이 비동기로 돌아가게 변경 (#104)
    * [#80] refactor: 메일 발송 로직이 비동기로 돌아가게 변경
    
    * [#94] feat: AsyncConfig 추가
    
    * [#94] feat: AsyncConfig 파일을 만들어서 @EnableAsync 어노테이션 Config 파일로 이동
    
    * [#94] refactor: 불필요한 import 삭제

--- schedule/src/main/java/itcast/mail/application/MailService.java
+++ schedule/src/main/java/itcast/mail/application/MailService.java
@@ -7,16 +7,18 @@ import itcast.mail.dto.request.EmailSender;
 import itcast.mail.dto.request.SendMailRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
+@Slf4j
 public class MailService {
 
     private final AmazonSimpleEmailService amazonSimpleEmailService;
     private final EmailSender emailSender;
 
+    @Async("taskExecutor")
     public void send(final SendMailRequest sendMailRequest) {
         try {
             final SendEmailRequest emailRequest = emailSender.from(sendMailRequest);
