f7f2027bbd2c0c90cdd5035b67be35fea4e9d391
shinheekim <shinhee1241@gmail.com>
2025-01-02 14:49:11 +0900

    fix: import 와일드카드로 수정

--- schedule/src/main/java/itcast/news/application/NewsService.java
+++ schedule/src/main/java/itcast/news/application/NewsService.java
@@ -1,8 +1,5 @@
 package itcast.news.application;
 
-import static itcast.exception.ErrorCodes.INVALID_NEWS_CONTENT;
-import static itcast.exception.ErrorCodes.NEWS_CRAWLING_ERROR;
-
 import itcast.ai.application.GPTService;
 import itcast.ai.dto.request.GPTSummaryRequest;
 import itcast.ai.dto.request.Message;
@@ -22,6 +19,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+
+import static itcast.exception.ErrorCodes.*;
 
 @Service
 @Slf4j
